40508b852e75f7f2b2e911a0d85289bbfa0f5d71
Jonas Thierbach <thierbach.jonas.181@gmail.com>
2022-06-29 23:43:06 +0200

    cards added

---
+++ src/lib/components/home/Card.svelte
@@ -0,0 +1,16 @@
+<script>
+    import './Card.scss'
+
+    export let name;
+    export let data;
+
+    export let icon;
+    export let line;
+</script>
+
+<div class="customCard">
+    <img id="icon" src={icon} alt="CardIcon">
+    <p>{name}</p>
+    <h3>{data}</h3>
+    <img id="line" src={line} alt="CardLine">
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
 <script>
-    import NavBar from '$lib/components/global/NavBar.svelte'
+    import Card from'$lib/components/home/Card.svelte'
     import '$lib/components/home/home.scss'
+    import FaTrophy from 'svelte-icons/fa/FaTrophy.svelte'
+    import FaCalendarAlt from 'svelte-icons/fa/FaCalendarAlt.svelte'
+    import FaDumbbell from 'svelte-icons/fa/FaDumbbell.svelte'
+    import MdAccountCircle from 'svelte-icons/md/MdAccountCircle.svelte'
+    import FaPlus from 'svelte-icons/fa/FaPlus.svelte'
     
     let menuStatus = false;
 
@@ -9,7 +14,8 @@
     }
 </script>
 
-<main>
+<div class="backgroundBlue"></div>
+<main style="font-family: 'Poppins', sans-serif;">
     <nav id="topNav">
         <div on:click={toggleMenu} class="burgerMenu">
             <div class={`line ${menuStatus && "active"}`}></div>
@@ -19,11 +25,22 @@
         <h3>ezzFit.com</h3>
         <img src="/images/global/profile.jpg" alt="Avatar">
     </nav>
-    <NavBar />
     <div id="imageContainer">
         <img src="/images/global/Wallpaper.png" alt="ezzFit" id="fitnessWallpaper">
     </div>
     <div class="background">
-
+        <div class="cards">
+            <div style="z-index: 100;"><Card name={"sd"} data={2000} icon={"/images/home/Calories.svg"} line={"/images/home/CaloriesLine.svg"}/></div>
+            <div style="z-index: 100;"><Card name={"sd"} data={2000} icon={"/images/home/Calories.svg"} line={"/images/home/CaloriesLine.svg"}/></div>
+            <div style="z-index: 100;"><Card name={"sd"} data={2000} icon={"/images/home/Calories.svg"} line={"/images/home/CaloriesLine.svg"}/></div>
+            <div style="z-index: 100;"><Card name={"sd"} data={2000} icon={"/images/home/Calories.svg"} line={"/images/home/CaloriesLine.svg"}/></div>
+        </div>
     </div>
+    <nav id="bottomNav">
+        <div class="trophyIcon ico"><FaTrophy /></div>
+        <div class="calenderIcon ico"><FaCalendarAlt /></div>
+        <div class="plusIcon"><div class="wrapper"><FaPlus /></div></div>
+        <div class="dumbellIcon ico"><FaDumbbell /></div>
+        <div class="accountIcon ico"><MdAccountCircle /></div>
+    </nav>
 </main>
